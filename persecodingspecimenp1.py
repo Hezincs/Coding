bee9fd21c4c60e9a454d2b8e521d06074718c87d
Hezincs <125618284+Hezincs@users.noreply.github.com>
2023-03-07 10:40:02 +0800

    Update persecodingspecimenp1.py

--- persecodingspecimenp1.py
+++ persecodingspecimenp1.py
@@ -1,8 +1,25 @@
 A = input()
 B = input()
 print(A + " " + B)
+#end
 
 A= input()
 B= input()
 print(int(A)-int(B))
+#end
+
+word = input()
+repeat = 50/len(word)
+result =""
+for i in range(int(repeat)):
+  result += word
+print(result)
+#end
+
+A = input()
+if int(A) > 50:
+  print("yes dream big")
+else:
+    print("on the small side")
+    #end
 
